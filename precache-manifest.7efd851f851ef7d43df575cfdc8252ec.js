bbc160572af9e0446d987d60b1366c609abe8fad
NareshKaushik <nareshkaushik36@gmail.com>
2019-12-27 14:19:51 +0530

    Updates

---
+++ precache-manifest.7efd851f851ef7d43df575cfdc8252ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2056c2ce9adf71ffe56eb7ed3e326e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e86d7eb8af55eba03e",
+    "url": "/static/css/main.4a125298.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98e86d7eb8af55eba03e",
+    "url": "/static/js/main.f6493e92.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  }
+]);
